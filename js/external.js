eb921b274a4711138634bb582eb34e90a4af5a0b
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-07 15:00:56 -0500

    Changes on the Javascript Intro Exercises

--- js/external.js
+++ js/external.js
@@ -10,18 +10,20 @@ alert("Great, " + favColor + " is my favorite color too!");
 
 // Exercise 3 section
 
+//Movies Rental
 var littleMermaid = parseInt(prompt("How many days you want to rent The Little Mermaid?"));
 var brotherBear = parseInt(prompt("How many days you want to rent Brother Bear?"));
 var hercules = parseInt(prompt("How many days you want to rent Hercules?"));
-var moviePerDay = parseInt(prompt("What is the rate per movie?"));
+var moviePerDay = 3;
 
 var totalRent = (littleMermaid + brotherBear + hercules) * moviePerDay;
 
 alert("The total rent of your movies is " + totalRent);
 
-var googleRate = parseInt(prompt("What is the rate for working at Google?"));
-var amazonRate = parseInt(prompt("What is the rate for working at Amazon?"));
-var facebookRate = parseInt(prompt("What is the rate for working at Facebook?"));
+//Contractor Pay Calculator
+var googleRate = 400;
+var amazonRate = 380;
+var facebookRate = 350;
 
 var googleHours = parseInt(prompt("How many hours worked at Google?"));
 var amazonHours = parseInt(prompt("How many hours worked at Amazon?"));
@@ -35,15 +37,17 @@ var totalPayment = totalGoogle + totalAmazon + totalFacebook;
 
 alert("Total payment is " + totalPayment);
 
+//Student class enrollment
 var classTime = prompt("What is your current class time?");
 var classSize = parseInt(prompt("What is the total class size?"));
 var currentClassSize = parseInt(prompt("What is the current class size?"));
 var myClass = prompt("What time is your current class?");
 
-var canSignUp = (currentClassSize < classSize) && (classTime != myClass);
+var canSignUp = (currentClassSize < classSize) && (classTime !== myClass);
 
 alert("The result if the student can sign up is " + canSignUp);
 
+//Discount
 var productOfferExpired = confirm("Is the product offer Expired?");
 var premium = confirm("Are you a Premium Member?");
 var cartItems = parseInt(prompt("How many items do you have on your cart?"));
