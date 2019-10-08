b2a3a17c7af333c19a7ed1146b05f18d106d22e9
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-08 14:53:08 -0500

    Extra Bonus exercises for Javascript Functions

--- js/functions-practice.js
+++ js/functions-practice.js
@@ -19,3 +19,26 @@ function division(x,y){
 console.log(multiplication(5, 3));
 console.log(division(10, 2));
 
+
+
+function getRandomItem(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var random = getRandomItem(15, 18);
+console.log(random);
+
+function rest(name){
+    return name.substring(1, name.length);
+}
+
+console.log(rest("eduardo"));
+
+function reverse(string){
+    return string.split("").reverse().join("");
+}
+
+console.log(reverse("eduardo"));
