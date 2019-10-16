45d027459165a748cfe369151e9f0d7f7e9962e2
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-16 09:01:40 -0500

    changes

--- js/while.js
+++ js/while.js
@@ -7,19 +7,19 @@
 //     number = number * 2;
 // }
 //
-// var allCones = Math.floor(Math.random()*50) + 50;
-// var conesSold;
-//
-// console.log("The total amount of cones is " + allCones);
-// do{
-//     conesSold = Math.floor(Math.random() * 5) + 1;
-//     if(conesSold <= allCones){
-//         console.log(conesSold + " cones Sold...");
-//         allCones = allCones - conesSold;
-//         console.log("Cones left: " + allCones);
-//     }else {
-//         console.log("Cannot sell you " + conesSold + " cones, I only have " + allCones + " ...");
-//     }
-// }while (allCones > 0)
-//
-// console.log("Yay! I sold them all!")
+var allCones = Math.floor(Math.random()*50) + 50;
+var conesSold;
+
+console.log("The total amount of cones is " + allCones);
+do{
+    conesSold = Math.floor(Math.random() * 5) + 1;
+    if(conesSold <= allCones){
+        console.log(conesSold + " cones Sold...");
+        allCones = allCones - conesSold;
+        console.log("Cones left: " + allCones);
+    }else {
+        console.log("Cannot sell you " + conesSold + " cones, I only have " + allCones + " ...");
+    }
+}while (allCones > 0)
+
+console.log("Yay! I sold them all!");
