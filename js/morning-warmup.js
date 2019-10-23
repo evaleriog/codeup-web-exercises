f40d0913b66fd4377f439f70c0df768f5785f6f0
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-10-23 17:03:42 -0500

    test

--- js/morning-warmup.js
+++ js/morning-warmup.js
@@ -194,7 +194,7 @@ function countUp(num){
 countUp(5);
 countUp(30);
 
-//morning warmup 10/23/19
+//morning warmup 10/23/19 h
 function createPersonObject(name, age){
     var person = {
         name: name,
