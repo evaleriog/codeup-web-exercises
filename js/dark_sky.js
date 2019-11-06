1528279a8cf1a88973c26bc361f733c6055b3665
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-11-05 22:05:43 -0600

    Adding map functionality in application

--- js/dark_sky.js
+++ js/dark_sky.js
@@ -1,70 +1,52 @@
 "use strict";
-var sanAntonioLatitude = 29.424349;
-var sanAntonioLongitude = -98.491142;
-var sanAntonioURL = createWeatherURL(sanAntonioLatitude, sanAntonioLongitude);
+const sanAntonioLatitude = 29.424349;
+const sanAntonioLongitude = -98.491142;
+const sanAntonioURL = createWeatherURL(sanAntonioLatitude, sanAntonioLongitude);
 
-var sanAntonioWeather = $.ajax(sanAntonioURL).done(function (data) {
-    console.log(data);
-    for(var x = 0; x < 3; x++){
+weatherCity(sanAntonioURL);
 
-        var day = data.daily.data[x];
-        var path = '';
+function weatherCity(url){
+    $.ajax(url).done(function (data) {
+        console.log(data);
 
-        switch (x) {
-            case 0:
-                path = '.today';
-                displayWeather(day, path);
-                break;
-            case 1:
-                path = '.tomorrow';
-               displayWeather(day, path);
-                break;
-            case 2:
-                path = '.after_tomorrow';
-                displayWeather(day, path);
-                break;
+        for(let x = 0; x < 3; x++) {
 
+            let day = data.daily.data[x];
+
+            displayWeather(day);
         }
+    }).fail(function () {
+        console.log("Connection to the API not working...");
+    });
+}
 
-    }
+function displayWeather(day){
+    let dateObject = new Date(day.time * 1000);
 
-}).fail(function () {
-    console.log("Connection to the API not working...");
-});
-
-function displayWeather(day, path){
-    var thePath = path;
-
-    var html = "<span>";
+    let html = "<div class='today card mb-3'><div class='row no-gutters'><div class='col-md-4'>";
+    html += "<img src='' class='card-img' alt=''>";
+    html += "</div><div class='col-md-8'><div class='card-body'>";
+    html += "<h5 class='card-title temperature'>";
     html += Math.round(day.temperatureHigh);
     html += "\xB0 / ";
     html += Math.round(day.temperatureLow);
-    html += "\xB0</span>"
-    path += ' .temperature' ;
-    $(path).html(html);
-
-    html = "Summary: <span>";
+    html += "\xB0";
+    html += "</h5><p class='card-text summary'>";
+    html += "Summary: <span>";
     html += day.summary;
     html += "</span>";
-    path = thePath + ' .summary';
-    $(path).html(html);
-
-    html = "Humidity: <span>";
+    html += "</p><p class='card-text humidity'>";
+    html += "Humidity: <span>";
     html += day.humidity;
-    html += "</span>";
-    path = thePath + ' .humidity';
-    $(path).html(html);
-
-    html = "Wind: <span>";
+    html += "</span></p><p class='card-text wind'>";
+    html += "Wind: <span>";
     html += day.windSpeed;
-    html += "</span>";
-    path = thePath + ' .wind';
-    $(path).html(html);
+    html += "</span></p><p class='card-text'><small class='text-muted date'>";
+    html += dateObject;
+    html += "</small></p></div></div></div></div>";
 
-    var dateObject = new Date(day.time * 1000);
-    html = "<p>" + dateObject + "</p>";
-    path = thePath + ' .date';
-    $(path).html(html);
+    $('.card-columns').append(html);
+
 }
 
 function createWeatherURL(latitude, longitude){
--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,8 @@
 "use strict";
+var address;
+
 mapboxgl.accessToken = mapboxToken;
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v9',
@@ -13,8 +16,23 @@ var marker = new mapboxgl.Marker({draggable: true})
     .addTo(map);
 
 function onDragEnd(){
-    var lngLat = marker.getLngLat();
-    console.log("Longitude: " + lngLat.lng + "  Latitue: " + lngLat.lat);
+    let lngLat = marker.getLngLat();
+    let url = createWeatherURL(lngLat.lat, lngLat.lng);
+
+    $('.card-columns').html("");
+    weatherCity(url);
+
+    reverseGeocode({lng: lngLat.lng, lat: lngLat.lat}, mapboxToken).then(function (results) {
+        address = results.split(',');
+        let r = "/\d+/";
+
+        let city = address[address.length - 3];
+        let state = address[address.length - 2].replace(/^(\w+)(\d{3})$/, " ");
+        let country = address[address.length - 1];
+
+        $('#place').text(city + ", " + state + ", " + country);
+    });
+
 }
 
 marker.on('dragend', onDragEnd);
